b7f9f8053364ffe5051055aa55711feb97a9f37e
evgeniyatyutyunik <evgeniya.tyutyunik@gmail.com>
2021-03-15 15:21:14 +0200

    Homework1 - unicode Hello added

---
+++ HM1/src/UnicodeHello.java
@@ -0,0 +1,13 @@
+public class UnicodeHello {
+    public static void main(String[] args){
+        char h = 72;
+        char e = 69;
+        char l = 76;
+        char o = 79;
+        System.out.println(h);
+        System.out.println(e);
+        System.out.println(l);
+        System.out.println(l);
+        System.out.println(o);
+    }
+}
