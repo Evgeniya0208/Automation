24e55667715466fe28ab1cc453709469bccfe0e3
evgeniyatyutyunik <evgeniya.tyutyunik@gmail.com>
2021-03-15 15:29:42 +0200

    Homework1 - Hello in one line

--- HM1/src/UnicodeHello.java
+++ HM1/src/UnicodeHello.java
@@ -4,10 +4,6 @@ public class UnicodeHello {
         char e = 69;
         char l = 76;
         char o = 79;
-        System.out.println(h);
-        System.out.println(e);
-        System.out.println(l);
-        System.out.println(l);
-        System.out.println(o);
+        System.out.println(h + "" + e + "" + l + "" + l + "" + o);
     }
 }
